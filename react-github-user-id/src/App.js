c2aca61795711087b061b0f46ed34da7b5def826
remoo1901 <remoo1901@hotmail.com>
2020-02-14 20:01:19 -0500

    second API calling on DidMount

--- react-github-user-id/src/App.js
+++ react-github-user-id/src/App.js
@@ -1,31 +1,31 @@
-import React from 'react';
-import axios from 'axios'
-import './App.css';
+import React from "react";
+import axios from "axios";
+import "./App.css";
 
-class App extends React.Component () {
-  constructor(){
-    super()
-    this.State ={
+class App extends React.Component() {
+  constructor() {
+    super();
+    this.State = {
       userData: {},
       followersData: []
-    }
+    };
   }
 
-  componentDidMount(){
-  axios 
-  .get('https://api.github.com/users/remoo1901')
-  .then( res => res.json())
-  .then(x => this.setState({userData: x.data}))
-  .catch(err => console.log(err))
+  componentDidMount() {
+    axios
+      .get("https://api.github.com/users/remoo1901")
+      .then(res => res.json())
+      .then(info => this.setState({ userData: info.data }))
+      .catch(err => console.log(err));
 
+    axios
+      .get("https://api.github.com/users/remoo1901/followers")
+      .then(x => this.setState({ followersData: x.data.followers }))
+      .catch(err2 => console.log(err2));
   }
-  
-  render(){
-  return (
-    <div className="App">
-      
-    </div>
-  );
+
+  render() {
+    return <div className="App"></div>;
   }
 }
 
