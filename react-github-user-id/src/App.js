1ba09eb92b22b7e181db66816a7515248898295e
remoo1901 <remoo1901@hotmail.com>
2020-02-14 21:54:45 -0500

    added github urls to user and followers

--- react-github-user-id/src/App.js
+++ react-github-user-id/src/App.js
@@ -2,11 +2,11 @@ import React from "react";
 import axios from "axios";
 import "./App.css";
 
-class App extends React.Component() {
+class App extends React.Component {
   constructor() {
     super();
-    this.State = {
-      userData: {},
+    this.state = {
+      userData: [],
       followersData: []
     };
   }
@@ -14,18 +14,48 @@ class App extends React.Component() {
   componentDidMount() {
     axios
       .get("https://api.github.com/users/remoo1901")
-      .then(res => res.json())
       .then(info => this.setState({ userData: info.data }))
       .catch(err => console.log(err));
 
     axios
       .get("https://api.github.com/users/remoo1901/followers")
-      .then(x => this.setState({ followersData: x.data.followers }))
-      .catch(err2 => console.log(err2));
+      .then(x => this.setState({ followersData: x.data }))
+      .catch(err => console.log(err));
   }
 
   render() {
-    return <div className="App"></div>;
+    return (
+      <div className="App">
+        <header>
+          <h1>Welcome to my GitHub Account</h1>
+        </header>
+        <h4>Login: {this.state.userData.login}</h4>
+        <h2>Name: {this.state.userData.name}</h2>
+        <img
+          width="200px"
+          height="200px"
+          src={this.state.userData.avatar_url}
+          alt="avatar"
+        />
+
+        <h1>My Followers</h1>
+        <div>
+          {this.state.followersData.map(follower => {
+            return (
+              <div>
+                <p>Login: {follower.login}</p>
+                <img
+                  width="200px"
+                  height="200px"
+                  src={follower.avatar_url}
+                  alt={follower.login}
+                />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
   }
 }
 
